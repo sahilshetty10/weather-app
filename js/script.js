d3074a196e7487d2b4f545e896767fa48eacf00f
Sahil Shetty <sahilshetty69@gmail.com>
2024-03-14 17:07:51 -0400

    Parse date

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ fetchData("toronto");
 async function fetchData(city) {
   let response = await fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=5c2cd8491d4f4186aea02645241403&q=${city}
-        &days=6&aqi=no&alerts=no`
+        &days=5&aqi=no&alerts=no`
   );
   let data = await response.json();
   console.log(data);
@@ -39,14 +39,19 @@ function updateForecast(data) {
   for (let index = 1; index < forecast_data.length; index++) {
     let forecast_day = forecast_data[index];
     let date = forecast_day.date;
-    console.log(date);
+    let formatted_date = new Date(date).toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    });
     let condition = forecast_day.day.condition.text;
     let max_temp = forecast_day.day.maxtemp_c;
     let min_temp = forecast_day.day.mintemp_c;
     let icon = forecast_day.day.condition.icon;
     let humidity = forecast_day.day.avghumidity;
     let windSpeed = forecast_day.day.maxwind_kph;
-    document.querySelector(`#day${index} > .forecast-date`).innerText = date;
+    document.querySelector(`#day${index} > .forecast-date`).innerText =
+      formatted_date;
     document.querySelector(
       `#day${index} > .forecast-details > .condition-container > .forecast-condition`
     ).innerText = condition;
